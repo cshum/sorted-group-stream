16f2cb1691a5e818698cf37e790433a22a87da63
Adrian Shum <me@cshum.com>
2015-09-16 22:57:08 +0800

    progress merge join test

--- test.js
+++ test.js
@@ -4,7 +4,7 @@ var callback = require('callback-stream')
 var join = require('./')
 var merge = require('./merge')
 
-test('merge sort', function (t) {
+test('merge', function (t) {
   var a = from.obj(['a', 'b', 'd', 'e', 'g', 'h'])
   var b = from.obj(['b', 'c', 'f'])
   merge(a, b).pipe(callback.obj(function (err, data) {
@@ -15,6 +15,7 @@ test('merge sort', function (t) {
     t.end()
   }))
 })
+
 test('join', function (t) {
   join(from.obj([
     {key: 1}, {key: 1}, {key: 2}, {key: 3}, {key: 3}
@@ -26,6 +27,28 @@ test('join', function (t) {
       [ { key: 2 } ],
       [ { key: 3 }, { key: 3 } ]
     ], 'join')
+    t.end()
+  }))
+})
+
+test('merge-join', function (t) {
+
+  function toKey (data) {
+    return data.id
+  }
+  var a = from.obj([{id: 1}, {id: 3}, {id: 6}])
+  var b = from.obj([{id: 1}, {id: 2}, {id: 6}])
+  var c = from.obj([{id: 3}, {id: 5}, {id: 6}])
+
+  join(a, b, c, toKey).pipe(callback.obj(function (err, data) {
+    t.notOk(err)
+    t.deepEqual(data, [
+      [{id: 1}, {id: 1}],
+      [{id: 2}],
+      [{id: 3}, {id: 3}],
+      [{id: 5}],
+      [{id: 6}, {id: 6}, {id: 6}]
+    ], 'merge join')
     t.end()
   }))
 })
